976737c600efceebef7c37820e0d1710a8b12c29
Eric RF <erojas@labelgrup.com>
2022-07-19 15:42:49 +0200

    Update Time helper

--- src/Helpers/Time.php
+++ src/Helpers/Time.php
@@ -15,13 +15,18 @@ class Time
     ): string
     {
         $secondsInAMinute = 60;
-        $secondsInAnHour  = 60 * $secondsInAMinute;
-        $secondsInADay    = 24 * $secondsInAnHour;
+        $secondsInAnHour = 60 * $secondsInAMinute;
+        $secondsInADay = 24 * $secondsInAnHour;
+        $secondsInAWeek = 7 * $secondsInADay;
 
+        // extract weeks
+        $weekSeconds = $inputSeconds % $secondsInAWeek;
+        $weeks = floor($inputSeconds / $secondsInAWeek);
         // extract days
-        $days = floor($inputSeconds / $secondsInADay);
+        $daySeconds = $weekSeconds % $secondsInAWeek;
+        $days = floor($daySeconds / $secondsInADay);
         // extract hours
-        $hourSeconds = $inputSeconds % $secondsInADay;
+        $hourSeconds = $daySeconds % $secondsInADay;
         $hours = floor($hourSeconds / $secondsInAnHour) + ($days * 24);
         // extract minutes
         $minuteSeconds = $hourSeconds % $secondsInAnHour;
@@ -32,16 +37,24 @@ class Time
 
         $timeForHumans = '';
 
-        if ( $unitMin === 's' ) {
-            $timeForHumans = $seconds . ' ' . __('seconds');
+        if ($unitMin === 's') {
+            $timeForHumans = $seconds . ' ' . __((int)$seconds === 1 ? 'second' : 'seconds');
         }
 
-        if ( $minutes && in_array($unitMin, ['s', 'i']) ) {
-            $timeForHumans = $minutes . ' ' . __('minutes') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
+        if ($minutes && in_array($unitMin, ['s', 'i'])) {
+            $timeForHumans = $minutes . ' ' . __((int)$minutes === 1 ? 'minute' : 'minutes') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
         }
 
-        if ( $hours ) {
-            $timeForHumans = $hours . ' ' . __('hours') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
+        if ($hours) {
+            $timeForHumans = $hours . ' ' . __((int)$hours === 1 ? 'hour' : 'hours') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
+        }
+
+        if ($days) {
+            $timeForHumans = $days . ' ' . __((int)$days === 1 ? 'day' : 'days') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
+        }
+
+        if ($weeks) {
+            $timeForHumans = $weeks . ' ' . __((int)$weeks === 1 ? 'week' : 'weeks') . (!empty($timeForHumans) ? ' ' . $timeForHumans : '');
         }
 
         return $timeForHumans;
