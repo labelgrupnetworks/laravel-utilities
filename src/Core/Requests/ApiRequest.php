7457c4b1d3005b1375cc8aca26aa9d6b4778ecb8
Eric RF <multimediospower@gmail.com>
2022-11-09 11:58:21 +0100

    Update ApiRequest + UseCase

--- src/Core/Requests/ApiRequest.php
+++ src/Core/Requests/ApiRequest.php
@@ -4,13 +4,14 @@ namespace Labelgrup\LaravelUtilities\Core\Requests;
 
 use Illuminate\Http\Exceptions\HttpResponseException;
 use Labelgrup\LaravelUtilities\Helpers\ApiResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 abstract class ApiRequest extends \Illuminate\Foundation\Http\FormRequest
 {
     protected function failedValidation(\Illuminate\Contracts\Validation\Validator $validator): void
     {
         if ($this->expectsJson()) {
-            throw new HttpResponseException(ApiResponse::fail(__('Validation Error'), $validator->errors()->toArray()));
+            throw new HttpResponseException(ApiResponse::fail(__('Validation Error'), $validator->errors()->toArray(), Response::HTTP_UNPROCESSABLE_ENTITY));
         }
 
         parent::failedValidation($validator);
